870fba126ce448ef73b5a1c1c447075d69a4d2cb
echortech <147915250+echortech@users.noreply.github.com>
2023-12-21 14:31:47 +0530

    Add files via upload

---
+++ tests/demo/navbar.spec.js
@@ -0,0 +1,17 @@
+
+// import { test, expect } from '@playwright/test';
+
+
+// test('test', async ({ page }) => {
+
+//   await page.goto('https://develop.d39yyvlacx19xd.amplifyapp.com/');
+//   await page.locator('div').filter({ hasText: /^Strategy BuilderSavedTempates$/ }).getByRole('img').first().isVisible();
+//   await page.getByText('Strategy Builder').click({ timeout: 30000});
+//   await page.locator('div').filter({ hasText: /^Strategy Builder$/ }).getByRole('img').click();
+//   await page.getByText('Saved').click();
+//   await page.getByText('Tempates').click();
+//   await page.locator('html').click();  
+//   await page.goto('https://develop.d39yyvlacx19xd.amplifyapp.com/savedstrategy');
+//   await expect(page.locator('div').filter({ hasText: /^Strategy BuilderSavedTempates$/ }).getByRole('img').first()).toBeVisible();
+// });
+
